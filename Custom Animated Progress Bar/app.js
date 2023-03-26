ffea291bd5713c92f06bfba89ebbc12e00e05903
Fahad-Iqbal <fahadiq@hotmail.com>
2023-03-25 22:01:08 -0400

    add scrolling progress number

--- Custom Animated Progress Bar/app.js
+++ Custom Animated Progress Bar/app.js
@@ -2,15 +2,29 @@ const progressBar = document.querySelector(".progress-done");
 const submit = document.querySelector('input[type="submit"]');
 
 submit.addEventListener("click", function (e) {
+  progressBar.style.width = 0 + "%";
+  progressBar.innerText = "";
   const input = document.querySelector('input[type="number"]');
   e.preventDefault();
   if (input.value <= 100 && input.value >= 0 && input.value.length > 0) {
     progress = input.value;
     setTimeout(function () {
-      progressBar.style.width = progress + "%";
       progressBar.style.opacity = 1;
-      progressBar.innerText = progress + "%";
-    }, 500);
+    }, 10);
+
+    async function sleepingFunc() {
+      for (let i = 0; i <= progress; i++) {
+        progressBar.style.width = i + "%";
+        progressBar.innerText = i + "%";
+        await sleep(Math.round(15));
+      }
+    }
+
+    sleepingFunc();
   }
   input.value = "";
 });
+
+function sleep(ms) {
+  return new Promise((resolveFunc) => setTimeout(resolveFunc, ms));
+}
