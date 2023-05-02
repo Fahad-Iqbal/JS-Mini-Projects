50a76a2420a56e13fdd91d40ecda0235db8ec0fb
Fahad-Iqbal <fahadiq@hotmail.com>
2023-05-02 19:03:41 -0400

    finish

--- The Comic Buddy App/app.js
+++ The Comic Buddy App/app.js
@@ -30,9 +30,68 @@ class SuperheroList {
       document.querySelector("#power").value,
     ] = ["", "", ""];
   }
+
+  validationError() {
+    document.querySelector(".validate-error").classList.add("show-validation");
+    setTimeout(() => {
+      document
+        .querySelector(".validate-error")
+        .classList.remove("show-validation");
+    }, 2500);
+  }
+
+  validationSuccess() {
+    document
+      .querySelector(".validate-success")
+      .classList.add("show-validation");
+    setTimeout(() => {
+      document
+        .querySelector(".validate-success")
+        .classList.remove("show-validation");
+    }, 1500);
+  }
+}
+
+class StoreSuperhero {
+  static getSuperhero() {
+    let superheroes;
+    if (localStorage.getItem("superheroes") === null) {
+      superheroes = [];
+    } else {
+      superheroes = JSON.parse(localStorage.getItem("superheroes"));
+    }
+    return superheroes;
+  }
+  static addSuperhero(entry) {
+    const superherosList = StoreSuperhero.getSuperhero();
+    superherosList.push(entry);
+    localStorage.setItem("superheroes", JSON.stringify(superherosList));
+  }
+
+  static displaySuperhero() {
+    const superheroesList = StoreSuperhero.getSuperhero();
+
+    superheroesList.forEach((superhero) => {
+      // Instantiating the SuperheroList Class
+      const list = new SuperheroList();
+      list.addSuperhero(superhero);
+    });
+  }
+
+  static removeSuperhero(clickedSuperhero) {
+    const superheroesList = StoreSuperhero.getSuperhero();
+    superheroesList.forEach((superhero, index) => {
+      if (superhero.superheroName === clickedSuperhero) {
+        superheroesList.splice(index, 1);
+      }
+    });
+    localStorage.setItem("superheroes", JSON.stringify(superheroesList));
+  }
 }
 
 // Events
+
+document.addEventListener("DOMContentLoaded", StoreSuperhero.displaySuperhero);
 
 const form = document.querySelector(".superhero-form");
 form.addEventListener("submit", function (e) {
@@ -51,6 +110,31 @@ form.addEventListener("submit", function (e) {
   );
   const list = new SuperheroList();
 
-  list.addSuperhero(entry);
-  list.clearSuperheroInputs();
+  if (
+    superheroName === "" ||
+    superheroUniverse === "" ||
+    superheroPower === ""
+  ) {
+    list.validationError();
+  } else {
+    list.addSuperhero(entry);
+    list.clearSuperheroInputs();
+    list.validationSuccess();
+
+    // Adding superhero to localStorage
+    StoreSuperhero.addSuperhero(entry);
+  }
+});
+
+// Deleting Listed Superheroes
+const listData = document.querySelector(".superhero-list-data");
+listData.addEventListener("click", function (e) {
+  if (e.target.className === "fas fa-trash") {
+    const trash = e.target.parentNode;
+    const clickedSuperhero =
+      e.target.previousElementSibling.previousElementSibling
+        .previousElementSibling.textContent;
+    StoreSuperhero.removeSuperhero(clickedSuperhero);
+    trash.remove();
+  }
 });
