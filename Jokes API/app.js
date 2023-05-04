6307876615707da64d5878d43b98003c5fd80298
Fahad-Iqbal <fahadiq@hotmail.com>
2023-05-04 17:37:23 -0400

    finish

--- Jokes API/app.js
+++ Jokes API/app.js
@@ -1,1 +1,16 @@
 // https://icanhazdadjoke.com/api
+const apiURL = "https://api.chucknorris.io/jokes/random";
+const jokeText = document.querySelector(".joke");
+const jokeBtn = document.querySelector(".joke-container button");
+
+jokeBtn.addEventListener("click", showJoke);
+
+async function showJoke() {
+  const responseData = await fetch(apiURL)
+    .then((response) => response.json())
+    .then((responseData) => {
+      return responseData.value;
+    });
+
+  jokeText.textContent = await responseData;
+}
