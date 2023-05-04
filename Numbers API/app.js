47b51aa3cdafc00d281906b68c0dfe9d1c42a3bc
Fahad-Iqbal <fahadiq@hotmail.com>
2023-05-04 18:19:11 -0400

    finish numbers API

--- Numbers API/app.js
+++ Numbers API/app.js
@@ -1,1 +1,13 @@
 // http://numbersapi.com/
+const numInput = document.querySelector(".fact-container input");
+const factBtn = document.querySelector(".fact-container button");
+const factText = document.querySelector(".fact");
+
+factBtn.addEventListener("click", async function () {
+  const number = numInput.value || 1;
+  const url = `http://numbersapi.com/${number}`;
+  const fact = await fetch(url).then((response) => {
+    return response.text();
+  });
+  factText.textContent = fact;
+});
