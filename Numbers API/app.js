701bf0cd0ab16347f20747afc0dfc8516cfc0fdf
Fahad-Iqbal <fahadiq@hotmail.com>
2023-05-04 18:26:05 -0400

    add https proxy

--- Numbers API/app.js
+++ Numbers API/app.js
@@ -6,7 +6,8 @@ const factText = document.querySelector(".fact");
 factBtn.addEventListener("click", async function () {
   const number = numInput.value || 1;
   const url = `http://numbersapi.com/${number}`;
-  const fact = await fetch(url).then((response) => {
+  const proxyURL = "https://cors-anywhere.herokuapp.com/";
+  const fact = await fetch(`${proxyURL}${url}`).then((response) => {
     return response.text();
   });
   factText.textContent = fact;
